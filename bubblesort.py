7cb423f7076a03d97002e32ea970d7afdf74c85b
PrazwalStark <prazwolstark@gmail.com>
2023-03-01 11:51:11 +0545

    Bubble sort edited

--- bubblesort.py
+++ bubblesort.py
@@ -1,11 +1,15 @@
 def bubble_sort(arr,n):
     for i in range(0,n-1):
+        flag = 0
         for j in range(0,n-1):
             if (arr[j]>arr[j+1]):
                 # temp=arr[j]
                 # arr[j]=arr[j+1]
                 # arr[j+1]=temp
+                flag=1
                 arr[j],arr[j+1]=arr[j+1],arr[j]
+        if (flag == 0):
+            break
     return arr
 arr=[]
 n = int(input("Enter the number of inputs?"))
